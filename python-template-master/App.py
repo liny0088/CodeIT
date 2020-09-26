401fb49d32da2a1c6ed3f519b4666320363eb5a2
liny0088 <45629155+liny0088@users.noreply.github.com>
2020-09-26 16:37:19 +0800

    square

--- python-template-master/App.py
+++ python-template-master/App.py
@@ -22,6 +22,5 @@ if __name__ == "__main__":
     logging.info("Starting application ...")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('localhost', 0))
-    port = sock.getsockname()[1]
     sock.close()
     app.run(port=5000)
