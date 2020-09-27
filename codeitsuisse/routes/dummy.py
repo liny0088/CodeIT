519213baaabd607e1e31825670b2968236c7d43c
liny0088 <45629155+liny0088@users.noreply.github.com>
2020-09-27 10:59:22 +0800

    5000

--- codeitsuisse/routes/dummy.py
+++ codeitsuisse/routes/dummy.py
@@ -8,5 +8,5 @@ logger = logging.getLogger(__name__)
 @app.route('/fruitbasket', methods=['POST'])
 def getdummy():
     data = request.get_json()
-    result = 15200
+    result = 5000
     return jsonify(result)
